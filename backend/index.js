7b16d96aa5b1b935e01946125452d3f7cf37ef17
Simran <itssimran903@gmail.com>
2024-01-24 23:42:48 +0530

    commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+
+const db_url = "mongodb+srv://new-user-31:04RKDEcmBt8YeWlu@cluster0.qjfafn9.mongodb.net/?retryWrites=true&w=majority"
+
+// Connection to database
+mongoose.connect(db_url)
+.then(() => console.log('connected to db'))
+.catch((error) => console.log(error))
+
+// Server
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on ${PORT}`)
+})
